15149fa2f7096b4f3230c604fb711d3b38400f06
M Nakul Kumar <nakul.m.kumar@gmail.com>
2021-12-30 11:51:50 +0530

    vowels4.py with frequency counts of vowels

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+person3 = { 'Name' : 'Ford Prefect',
+			'Gender' : 'Male',
+			'Occupation' : 'Researcher',
+			'Home Planet' : 'Betelgeuse Seven' }
+
+
+found = {}
+
+found['a'] = 0
+found['e'] = 0
+found['i'] = 0
+found['o'] = 0
+found['u'] = 0
