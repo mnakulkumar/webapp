4ad123d9da620a02c3bb68ad706cccf615b49fd8
M Nakul Kumar <nakul.m.kumar@gmail.com>
2022-01-15 11:34:02 +0530

    adding the url /entry to webapp

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -11,4 +11,8 @@ def hello() -> str:
 def do_search() -> str:
 	return(str(search4letters('life, the universe, and everything!', 'eiru,!')))
 
+@app.route('/entry')
+def entry_page() -> 'html':
+	return render_template('entry.html', the_title ='Welcome to search4letters on the web!')
+	
 app.run()
