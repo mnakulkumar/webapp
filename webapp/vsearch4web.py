41b1da8fff34cff4de3bce0ed91dcdaf1dc6ed2e
M Nakul Kumar <nakul.m.kumar@gmail.com>
2022-01-23 16:30:03 +0530

    Using sep argument in print

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 def log_request(req: 'flask_request', res: str) -> None:
 	with open('vsearch.log', 'a') as log:
 		#print(str(dir(req)), res, file = log)
-		print(req.form, file=log, end='|')
+		"""print(req.form, file=log, end='|')
 		print(req.remote_addr, file=log, end='|')
 		print(req.user_agent, file=log, end='|')
-		print(res, file=log)
+		print(res, file=log)"""
+		print(req.form, req.remote_addr, req.user_agent, res, file=log , sep='|')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
