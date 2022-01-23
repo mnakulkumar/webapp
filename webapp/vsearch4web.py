53444064bd71edbd255b2e753810b34b231f6262
M Nakul Kumar <nakul.m.kumar@gmail.com>
2022-01-23 10:55:03 +0530

    Stringified version of calling dir on each web request

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
 
 def log_request(req: 'flask_request', res :str) -> None:
 	with open('vsearch.log', 'a') as log:
-		print(req, res, file = log)
+		print(str(dir(req)), res, file = log)
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -30,7 +30,7 @@ def entry_page() -> 'html':
 def view_the_log() -> str:
 	with open('vsearch.log' ) as log:
 		contents = log.read()
-		return contents
+		return escape(contents)
 
 	
 if __name__ == '__main__':
