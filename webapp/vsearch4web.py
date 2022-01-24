c19412ddedd39f18d0ce5dab8a1b68e936b04dd4
M Nakul Kumar <nakul.m.kumar@gmail.com>
2022-01-24 21:07:39 +0530

    Reading logged data into list of lists

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -30,12 +30,19 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
 	return render_template('entry.html', the_title ='Welcome to search4letters on the web!')
 
-
-@app.route('/viewlog')
-def view_the_log() -> str:
+"""def view_the_log() -> str:
 	with open('vsearch.log' ) as log:
 		contents = log.read()
-		return escape(contents)
+		return escape(contents)"""
+@app.route('/viewlog')
+def view_the_log() -> str:
+	contents = []
+	with open('vsearch.log') as log:
+		for line in log:
+			contents.append([])
+			for item in line.split('|'):
+				contents[-1].append(escape(item))
+	return str(contents)
 
 	
 if __name__ == '__main__':
